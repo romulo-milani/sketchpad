f9f1ccfaaeb6ba41f2163a3be7c6fc4d49be1d9d
romulo.milani <81986766+romulo-milani@users.noreply.github.com>
2025-01-20 06:33:52 -0300

    implementado botão que muda o tamanho do grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let container = document.querySelector(".container");
+let btnAjustSize = document.querySelector(".ajustar-tamanho");
 
 //CRIA O GRID DE QUADRADOS
 for (i = 0; i < 256; i++) {
@@ -14,10 +15,43 @@ for (let i = 0; i < square.length; i++) {
     square[i].addEventListener("mouseenter", changeBackgroundColor);
 }
 
+//ADICIONA O EVENTO PARA O USUÁRIO MUDAR O TAMANHO DO GRID
+btnAjustSize.addEventListener("click", adjustSize);
+
+
 function changeBackgroundColor(e) {
     const red = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
 
     e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}
+
+function adjustSize () {
+    let test = true;
+    while (test) {
+        let newSize = prompt("Digite o novo tamanho: (máximo de 50)");
+        if(newSize <= 50 && newSize > 0) {
+            test = false;
+            let squares = document.getElementsByClassName("square");
+
+            //remove todos os elementos
+            while (squares.length > 0) {
+                squares[0].parentNode.removeChild(squares[0]);
+            }
+
+            //CRIA O NOVO GRID
+            for (i = 0; i < newSize * newSize; i++) {
+                let square = document.createElement("div");
+                square.classList.add("square");
+                square.style.width = `${550/newSize}px`;
+                square.style.height = `${550/newSize}px`;
+                square.addEventListener("mouseenter", changeBackgroundColor);
+                container.appendChild(square);
+            }
+
+
+        }
+    }
+    
 }
